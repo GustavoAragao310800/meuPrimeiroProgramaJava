8afbf52afcd845721dee3bb6b8d51257d2fd3927
GustavoAragao310800 <78882947+GustavoAragao310800@users.noreply.github.com>
2021-02-10 19:21:56 -0300

    Add files via upload

---
+++ meuprimeiroprogramajava/src/meuprimeiroprogramajava/principallllll.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package meuprimeiroprogramajava;
+
+/**
+ * @author MariaLucia
+ *
+ */
+public class principallllll {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
